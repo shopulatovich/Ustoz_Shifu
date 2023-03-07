19f07cffeb01dd9ff4744c9b000daf746a2693bc
Shopulatovich <Sherzod299@gmail.com>
2023-03-08 00:37:59 +0300

    new Git project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Git and gitHub'),
+        ),
+        body: Center(child: Text("Hello Git")),
+      ),
+    );
+  }
+}
